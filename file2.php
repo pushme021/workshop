0caca618b8aec97ad280f46fdb27d070f4fb5c85
push me <pushme021@tutamail.com>
2021-09-05 09:45:16 +0430

    add vendor1-bootstrap js to project

--- file2.php
+++ file2.php
@@ -1,4 +1,5 @@
 <!doctype html><html class="no-js" dir="ltr" loc="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-</script><script defer="defer" src="/bootstrap_qr.428c60993dba5c1514a3.js"></script>
+<script defer="defer" src="/vendor1~bootstrap_qr.8d5b7620b726968ba154.js"></script>
+<script defer="defer" src="/bootstrap_qr.428c60993dba5c1514a3.js"></script>
 s</body></html>
